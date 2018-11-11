9f652b05255907cb8bca0511794a381d23a6c906
stager <stager76@gmail.com>
2018-11-11 22:21:18 +0200

    added App.js

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import './styles/App.css'
+
+class App extends React.Component {
+    render() {
+        return (
+            <h2>
+                hello, strangers!!!
+            </h2>
+        )
+    }
+}
+
+export default App
